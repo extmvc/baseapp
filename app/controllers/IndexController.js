9b939469bc248ed6cb93e597da17855cdbdcbe7a
Ed Spencer <edward@midlandswebdesign.com>
2009-07-28 00:03:39 +0100

    Added basic action to index controller

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -4,5 +4,14 @@
  * Default root controller
  */
 MyApp.controllers.IndexController = Ext.extend(MyApp.controllers.ApplicationController, {
-  name: 'index'
+  name: 'index',
+  
+  /**
+   * Renders the home page into the main TabPanel
+   */
+  index: function() {
+    var view = this.render("Index");
+    
+    this.addTo.setActiveTab(view);
+  }
 });
